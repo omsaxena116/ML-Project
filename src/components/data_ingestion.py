4a2b2c1f2520dbeeed3cc8e39b40b0aa70abf33d
omsaxena116 <saxenaom62@gmail.com>
2026-07-17 13:39:58 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,10 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass # this decorator is useful coz we need not to use init to define a variable
 class DataIngestionConfig: #in my data ingestion component any input that is required , i will give thorugh this
     train_data_path:str=os.path.join('artifacts',"train.csv")#all the outputs will be stored in artifact folder and train.csv will be file name
@@ -53,3 +57,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
